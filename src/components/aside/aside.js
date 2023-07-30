eb4981dfd15ee0a3472fc7165dc80b42e23b5756
Yevhen Barkov <mr.e.barkov@gmail.com>
2023-07-30 23:07:25 +0300

    Final commit

---
+++ src/components/aside/aside.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./aside.css";
+
+const Aside = () => {
+  return (
+    <div className="aside-container">
+      <h2>Aside</h2>
+    </div>
+  );
+};
+
+export default Aside;
---
+++ src/components/block/block.js
@@ -0,0 +1,13 @@
+import "./block.css";
+
+const Block = () => {
+  return (
+    <div className="block-container">
+        <h2>
+          Main content here
+        </h2>
+    </div>
+  );
+};
+
+export default Block;
